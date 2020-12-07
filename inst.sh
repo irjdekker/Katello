0419ddf35aa8514546ae8e73117ffc93d0d49790
irjdekker <ir.j.dekker@gmail.com>
2020-12-07 16:38:06 +0100

    Update

--- inst.sh
+++ inst.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ## The easiest way to get the script on your machine is:
-## wget -O - https://raw.githubusercontent.com/irjdekker/Katello/master/inst.sh 2>/dev/null | bash
-echo
-echo -n Password: 
-read -s password
-echo
+## wget -O - https://raw.githubusercontent.com/irjdekker/Katello/master/inst.sh 2>/dev/null | bash -s <password>
+if [[ $# -eq 0 ]]
+then
+    echo "No password supplied"
+    exit
+fi
 localectl set-locale LC_CTYPE=en_US.utf8
 localectl status
 hostnamectl status
@@ -44,7 +45,7 @@ mv /etc/foreman-installer/scenarios.d/katello-answers.yaml /etc/foreman-installe
 wget -P /etc/foreman-installer/scenarios.d/ https://raw.githubusercontent.com/irjdekker/Katello/master/katello-answers.yaml
 chown root:root /etc/foreman-installer/scenarios.d/katello-answers.yaml
 chmod 600 /etc/foreman-installer/scenarios.d/katello-answers.yaml
-foreman-installer --scenario katello --foreman-initial-admin-username admin --foreman-initial-admin-password "$password"
+foreman-installer --scenario katello --foreman-initial-admin-username admin --foreman-initial-admin-password "$1"
 cat /var/log/foreman-installer/katello.log | grep -e "ERROR"
 foreman-maintain service status
 yum install open-vm-tools -y
@@ -55,14 +56,14 @@ cd /etc/pki/rpm-gpg/import/
 wget -P /etc/pki/rpm-gpg/import/ http://mirror.centos.org/centos-7/7/os/x86_64/RPM-GPG-KEY-CentOS-7
 hammer gpg create --organization-id 1 --key "RPM-GPG-KEY-CentOS-7" --name "RPM-GPG-KEY-CentOS-7"
 hammer repository create --organization-id 1 --product "CentOS 7 Linux x86_64" --name "CentOS 7 OS x86_64" --label "CentOS_7_OS_x86_64" --content-type "yum" \
---download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.centos.org/centos-7/7/os/x86_64/" --mirror-on-sync "no"
+--download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.ams1.nl.leaseweb.net/centos/7/os/x86_64/" --mirror-on-sync "no"
 hammer repository create --organization-id 1 --product "CentOS 7 Linux x86_64" --name "CentOS 7 Extra x86_64" --label "CentOS_7_Extra_x86_64" --content-type "yum" \
---download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.centos.org/centos-7/7/extras/x86_64/" --mirror-on-sync "no"
+--download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.ams1.nl.leaseweb.net/centos/7/extras/x86_64/" --mirror-on-sync "no"
 hammer repository create --organization-id 1 --product "CentOS 7 Linux x86_64" --name "CentOS 7 Updates x86_64" --label "CentOS_7_Updates_x86_64" --content-type "yum" \
---download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.centos.org/centos-7/7/updates/x86_64/" --mirror-on-sync "no"
+--download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.ams1.nl.leaseweb.net/centos/7/updates/x86_64/" --mirror-on-sync "no"
 hammer repository create --organization-id 1 --product "CentOS 7 Linux x86_64" --name "CentOS 7 Ansible x86_64" --label "CentOS_Ansible_x86_64" --content-type "yum" \
---download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.centos.org/centos-7/7/configmanagement/x86_64/ansible-29/" --mirror-on-sync "no"
-hammer sync-plan create --organization-id 1 --name "Daily Sync" --interval daily --enabled true –-sync-date "2020-12-06 02:30:00"
+--download-policy "immediate" --gpg-key "RPM-GPG-KEY-CentOS-7" --url "http://mirror.ams1.nl.leaseweb.net/centos/7/configmanagement/x86_64/ansible-29/" --mirror-on-sync "no"
+hammer sync-plan create --organization-id 1 --name "Daily Sync" --interval daily --enabled true --sync-date "2020-12-06 02:30:00" 
 hammer product set-sync-plan --organization-id 1 --name "CentOS 7 Linux x86_64" --sync-plan "Daily Sync"
 hammer product synchronize --organization-id 1 --name "CentOS 7 Linux x86_64"
 hammer lifecycle-environment create --organization-id 1 --name "Development" --label "Development" --prior "Library"
@@ -88,7 +89,7 @@ hammer activation-key add-subscription --organization-id 1 --name "CentOS_7_Test
 hammer activation-key add-subscription --organization-id 1 --name "CentOS_7_Acceptance_Key" --quantity "1" --subscription-id "1"
 hammer activation-key add-subscription --organization-id 1 --name "CentOS_7_Production_Key" --quantity "1" --subscription-id "1"
 hammer compute-resource create --organization-id 1 --location-id 2 --name "Tanix vCenter" --provider "Vmware" --server "vcenter.tanix.nl" \
---user "administrator@tanix.local" --password "$password" --datacenter "Datacenter"
+--user "administrator@tanix.local" --password "$1" --datacenter "Datacenter"
 cluster_id=$(hammer compute-resource clusters --organization-id 1 --location-id 2 --name "Tanix vCenter" | sed '4q;d' | cut -d '|' -f 1 | awk '{$1=$1};1')
 cluster=$(hammer compute-resource clusters --organization-id 1 --location-id 2 --name "Tanix vCenter" | sed '4q;d' | cut -d '|' -f 2 | awk '{$1=$1};1')
 network_id=$(hammer compute-resource networks --organization-id 1 --location-id 2 --name "Tanix vCenter" | sed '6q;d' | cut -d '|' -f 1 | awk '{$1=$1};1')
@@ -99,6 +100,7 @@ os_id=$(hammer os list | sed '4q;d' | cut -d '|' -f 1 | awk '{$1=$1};1')
 os=$(hammer os list | sed '4q;d' | cut -d '|' -f 2 | awk '{$1=$1};1')
 capsule_id=$(hammer capsule list | sed '4q;d' | cut -d '|' -f 1 | awk '{$1=$1};1')
 capsule=$(hammer capsule list | sed '4q;d' | cut -d '|' -f 2 | awk '{$1=$1};1')
+echo "$cluster_id ($cluster) -- $network_id ($network) -- $domain_id ($domain) -- $os_id ($os) -- $capsule_id ($capsule)"
 hammer compute-profile values create --organization-id 1 --location-id 2 --compute-profile "1-Small" --compute-resource "Tanix vCenter" \
 --compute-attributes cpus=1,corespersocket=1,memory_mb=2048,cluster=$cluster,memoryHotAddEnabled=1,cpuHotAddEnabled=1 \
 --volume datastore="Datastore Non-SSD",size_gb=30,thin=true --interface compute_type=VirtualVmxnet3,compute_network=$network_id
@@ -108,14 +110,15 @@ hammer compute-profile values create --organization-id 1 --location-id 2 --compu
 hammer compute-profile values create --organization-id 1 --location-id 2 --compute-profile "3-Large" --compute-resource "Tanix vCenter" \
 --compute-attributes cpus=1,corespersocket=2,memory_mb=4096,cluster=$cluster,memoryHotAddEnabled=1,cpuHotAddEnabled=1 \
 --volume datastore="Datastore Non-SSD",size_gb=30,thin=true --interface compute_type=VirtualVmxnet3,compute_network=$network_id
-hammer subnet create --organization-id 1 --location-id 2 --domain-ids $domain_id --name $network --network-type "IPv4" --network "10.10.5.0" \
+hammer subnet create --organization-id 1 --location-id 2 --domain-ids "$domain_id" --name "$network" --network-type "IPv4" --network "10.10.5.0" \
 --prefix 24 --gateway "10.10.5.1" --dns-primary "10.10.5.1" --boot-mode "Static" 
 hammer hostgroup create --organization-id 1 --location-id 2 --name "hg_production" --lifecycle-environment "Production" \
---content-view "CentOS 7" --content-source $capsule --compute-resource "Tanix vCenter" --compute-profile "1-Small" --domain-id $domain_id \
---subnet $network --architecture "x86_64" --operatingsystem $os --partition-table "Kickstart default"
+--content-view "CentOS 7" --content-source "$capsule" --compute-resource "Tanix vCenter" --compute-profile "1-Small" --domain-id "$domain_id" \
+--subnet "$network" --architecture "x86_64" --operatingsystem "$os" --partition-table "Kickstart default"
 hammer hostgroup set-parameter --hostgroup "hg_production" --name "kt_activation_keys" --value "CentOS_7_Production_Key"
+mkdir /var/lib/foreman/bootdisk
 yum install shim-x64 -y
-/usr/bin/cp -f /boot/efi/EFI/centos/shimx64.efi /var/lib/foreman/bootdisk/
+/usr/bin/cp -f /boot/efi/EFI/centos/shimx64.efi /var/lib/foreman/bootdisk/shimx64.efi
 yum install grub2-efi-x64 -y
-/usr/bin/cp -f /boot/efi/EFI/centos/grubx64.efi /var/lib/foreman/bootdisk/
+/usr/bin/cp -f /boot/efi/EFI/centos/grubx64.efi /var/lib/foreman/bootdisk/grubx64.efi
 read -s -n 1 -p "Press any key to continue . . ."
