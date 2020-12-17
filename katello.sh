7b155e838841f650d4fd57905521358e48829740
irjdekker <ir.j.dekker@gmail.com>
2020-12-17 08:04:05 +0100

    Update katello.sh

--- katello.sh
+++ katello.sh
@@ -564,7 +564,7 @@ do_function "Register katello host" "do_register_katello"
 do_task "Change destroy setting" "hammer settings set --name \"destroy_vm_on_host_delete\" --value \"yes\""
 
 # Create test host
-do_function "Create test host" "do_create_host \"awk\" \"hg_production/hg_tanix/hg_infra_$OS_NICE\" \"10.10.5.37\" \"$PASSWORD\" \"2-Medium\""
+do_function "Create test host" "do_create_host \"awk\" \"hg_production/hg_tanix/hg_infra_7_x\" \"10.10.5.37\" \"$PASSWORD\" \"2-Medium\""
 
 # Restore cursor
 tput cvvis
