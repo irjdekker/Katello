fdfef7943c2a76a47e326b502c9ae3ad4c5ec9fa
irjdekker <ir.j.dekker@gmail.com>
2020-12-19 17:21:46 +0100

    Update katello.sh

--- katello.sh
+++ katello.sh
@@ -121,8 +121,8 @@ do_install_katello() {
 }
 
 do_compute_resource() {
-    do_function_task "hammer compute-resource create --organization-id 1 --location-id 2 --name \"Tanix vCenter\" --provider \"Vmware\" --server \"vcenter.tanix.nl\" --user \"administrator@tanix.local\" --password \"${PASSWORD}\" --datacenter \"Datacenter\" --caching-enabled 1 --set-console-password 1"
-    do_function_task "curl -u admin:${PASSWORD} -H 'Content-Type:application/json' -H 'Accept:application/json' -k https://katello.tanix.nl/api/compute_resources/1-Tanix%20vCenter/refresh_cache -X PUT"
+    do_function_task "hammer compute-resource create --organization-id 1 --location-id 2 --name \"Tanix vCenter\" --provider \"Vmware\" --server \"vcenter.tanix.nl\" --user \"administrator@tanix.local\" --password \"${PASSWORD}\" --datacenter \"Datacenter\" --caching-enabled 0 --set-console-password 1"
+    #do_function_task "curl -u admin:${PASSWORD} -H 'Content-Type:application/json' -H 'Accept:application/json' -k https://katello.tanix.nl/api/compute_resources/1-Tanix%20vCenter/refresh_cache -X PUT"
 }
 
 do_compute_profiles() {
@@ -131,14 +131,14 @@ do_compute_profiles() {
     local network_id
     network_id=$(hammer --no-headers compute-resource networks --organization-id 1 --location-id 2 --name "Tanix vCenter" --fields Id,Name | grep "tanix-5" | cut -d '|' -f 1 | awk '{$1=$1};1')
 
-    do_function_task "hammer compute-profile delete --name \"1-Small\""
-    do_function_task "hammer compute-profile create --name \"1-Small\""
+    #do_function_task "hammer compute-profile delete --name \"1-Small\""
+    #do_function_task "hammer compute-profile create --name \"1-Small\""
     do_function_task "hammer compute-profile values create --organization-id 1 --location-id 2 --compute-profile \"1-Small\" --compute-resource \"Tanix vCenter\" --compute-attributes cpus=1,corespersocket=1,memory_mb=2048,firmware=automatic,cluster=${cluster},resource_pool=Resources,path=\"/Datacenters/Datacenter/vm\",guest_id=centos7_64Guest,hardware_version=Default,memoryHotAddEnabled=1,cpuHotAddEnabled=1,add_cdrom=0,boot_order=[disk],scsi_controller_type=VirtualLsiLogicController --volume name=\"Hard disk\",mode=persistent,datastore=\"Datastore Non-SSD\",size_gb=30,thin=true --interface compute_type=VirtualVmxnet3,compute_network=${network_id}"
-    do_function_task "hammer compute-profile delete --name \"2-Medium\""
-    do_function_task "hammer compute-profile create --name \"2-Medium\""
+    #do_function_task "hammer compute-profile delete --name \"2-Medium\""
+    #do_function_task "hammer compute-profile create --name \"2-Medium\""
     do_function_task "hammer compute-profile values create --organization-id 1 --location-id 2 --compute-profile \"2-Medium\" --compute-resource \"Tanix vCenter\" --compute-attributes cpus=2,corespersocket=1,memory_mb=2048,firmware=automatic,cluster=${cluster},resource_pool=Resources,path=\"/Datacenters/Datacenter/vm\",guest_id=centos7_64Guest,hardware_version=Default,memoryHotAddEnabled=1,cpuHotAddEnabled=1,add_cdrom=0,boot_order=[disk],scsi_controller_type=VirtualLsiLogicController --volume name=\"Hard disk\",mode=persistent,datastore=\"Datastore Non-SSD\",size_gb=30,thin=true --interface compute_type=VirtualVmxnet3,compute_network=${network_id}"
-    do_function_task "hammer compute-profile delete --name \"3-Large\""
-    do_function_task "hammer compute-profile create --name \"3-Large\""
+    #do_function_task "hammer compute-profile delete --name \"3-Large\""
+    #do_function_task "hammer compute-profile create --name \"3-Large\""
     do_function_task "hammer compute-profile values create --organization-id 1 --location-id 2 --compute-profile \"3-Large\" --compute-resource \"Tanix vCenter\" --compute-attributes cpus=2,corespersocket=1,memory_mb=4096,firmware=automatic,cluster=${cluster},resource_pool=Resources,path=\"/Datacenters/Datacenter/vm\",guest_id=centos7_64Guest,hardware_version=Default,memoryHotAddEnabled=1,cpuHotAddEnabled=1,add_cdrom=0,boot_order=[disk],scsi_controller_type=VirtualLsiLogicController --volume name=\"Hard disk\",mode=persistent,datastore=\"Datastore Non-SSD\",size_gb=30,thin=true --interface compute_type=VirtualVmxnet3,compute_network=${network_id}"
 }
 
