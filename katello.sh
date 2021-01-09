11a601a92bd759d3394d442eecdd1d5a25c75521
irjdekker <ir.j.dekker@gmail.com>
2021-01-09 22:38:26 +0100

    Update katello.sh

--- katello.sh
+++ katello.sh
@@ -60,9 +60,13 @@ ENCSOURCEFILE="$SOURCEFILE.enc"
 ## *************************************************************************************************** ##
 
 do_download_configfile() {
+    echo "1"
     do_function_task "curl -s https://raw.githubusercontent.com/irjdekker/Katello/master/source.sh.enc -o \"${ENCSOURCEFILE}\""
+    echo "2"
     do_function_task "/usr/bin/openssl enc -aes-256-cbc -d -in \"${ENCSOURCEFILE}\" -out \"{$SOURCEFILE}\" -pass pass:\"${PASSWORD}\""
+    echo "3"
     do_function_task "[ -f \"${ENCSOURCEFILE}\" ] && rm -f \"${ENCSOURCEFILE}\" || sleep 0.1"
+    echo "4"
     do_function_task "chmod 700 \"${SOURCEFILE}\""
 }
 
