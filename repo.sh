9bb6d46b2f92b7b46cbcf01300023773316f4999
irjdekker <ir.j.dekker@gmail.com>
2021-02-06 14:45:20 +0100

    Updates

--- repo.sh
+++ repo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC2181
+# shellcheck disable=SC1090
 
 ## The easiest way to get the script on your machine is:
 ## a) without specifying the version
 ## curl -s https://raw.githubusercontent.com/irjdekker/Katello/master/repo.sh -o repo.sh 2>/dev/null && bash repo.sh && rm -f repo.sh
-## wget -O repo.sh https://raw.githubusercontent.com/irjdekker/Katello/master/repo.sh 2>/dev/null && bash repo.sh && rm -f repo.sh
 ## b) with specifying the version
 ## curl -s https://raw.githubusercontent.com/irjdekker/Katello/master/repo.sh 2>/dev/null | bash -s <org_id> <version>
-## wget -O - https://raw.githubusercontent.com/irjdekker/Katello/master/repo.sh 2>/dev/null | bash -s <org_id> <version>
 
 ## *************************************************************************************************** ##
 ##      __      __     _____  _____          ____  _      ______  _____                                ##
@@ -21,6 +20,8 @@
 ##
 ## The following variables are defined below
 
+SCRIPT_NAME="repo"
+COMMAND_DEBUG=true
 OSSETUP=('8.x,8.3,5,http://mirror.1000mbps.com/centos/8/,BaseOS,BaseOS/x86_64/os/,AppStream,AppStream/x86_64/os/,PowerTools,PowerTools/x86_64/os/,Extras,extras/x86_64/os/,Ansible,configmanagement/x86_64/ansible-29/' \
 '8.2,8.2,5,http://mirror.1000mbps.com/centos-vault/8.2.2004/,BaseOS,BaseOS/x86_64/os/,AppStream,AppStream/x86_64/os/,PowerTools,PowerTools/x86_64/os/,Extras,extras/x86_64/os/,Ansible,configmanagement/x86_64/ansible-29/' \
 '8.1,8.1,5,http://mirror.1000mbps.com/centos-vault/8.1.1911/,BaseOS,BaseOS/x86_64/os/,AppStream,AppStream/x86_64/os/,PowerTools,PowerTools/x86_64/os/,Extras,extras/x86_64/os/,Ansible,configmanagement/x86_64/ansible-29/' \
@@ -34,12 +35,6 @@ OSSETUP=('8.x,8.3,5,http://mirror.1000mbps.com/centos/8/,BaseOS,BaseOS/x86_64/os
 '7.3,7.3,3,http://mirror.1000mbps.com/centos-vault/7.3.1611/,OS,os/x86_64/,Extras,extras/x86_64/,Updates,updates/x86_64/' \
 '7.2,7.2,3,http://mirror.1000mbps.com/centos-vault/7.2.1511/,OS,os/x86_64/,Extras,extras/x86_64/,Updates,updates/x86_64/' \
 '7.1,7.1,3,http://mirror.1000mbps.com/centos-vault/7.1.1503/,OS,os/x86_64/,Extras,extras/x86_64/,Updates,updates/x86_64/')
-LOGFILE="$HOME/repo-install-$(date +%Y-%m-%d_%Hh%Mm).log"
-IRed='\e[0;31m'
-IGreen='\e[0;32m'
-IYellow='\e[0;33m'
-Reset='\e[0m'
-VMWARE="vmware_home"
 
 ## *************************************************************************************************** ##
 ##       _____   ____  _    _ _______ _____ _   _ ______  _____                                        ##
@@ -174,122 +169,6 @@ do_populate_katello() {
     done < <(hammer --no-headers location list --fields Name)
 }
 
-print_padded_text() {
-    pad=$(printf '%0.1s' "*"{1..70})
-    padlength=140
-    updatetext=" $(date -u ) - $1 "
-    padleft=$(( (padlength - ${#updatetext}) / 2 ))
-    padright=$(( padlength - ${#updatetext} - padleft ))
-
-    printf '%*.*s' 0 $padleft "${pad}"
-    printf '%s' "$updatetext"
-    printf '%*.*s' 0 $padright "${pad}"
-    printf '\n'
-}
-
-print_task() {
-    local TEXT
-    TEXT="$1"
-    local STATUS
-    STATUS="$2"
-    local NEWLINE
-    NEWLINE="$3"
-
-    if (( STATUS == -3 )); then
-        PRINTTEXT="\r[ ${IYellow}WARN${Reset} ] "
-    elif (( STATUS == -2 )); then
-        PRINTTEXT="\r         "
-    elif (( STATUS == -1 )); then
-        print_padded_text "${TEXT}" >> "${LOGFILE}"
-        PRINTTEXT="\r[      ] "
-    elif (( STATUS == 0 )); then
-        PRINTTEXT="\r[  ${IGreen}OK${Reset}  ] "
-    elif (( STATUS >= 1 )); then
-        PRINTTEXT="\r[ ${IRed}FAIL${Reset} ] "
-    else
-        PRINTTEXT="\r         "
-    fi
-
-    PRINTTEXT+="${TEXT}"
-
-    if [ "${NEWLINE}" = "true" ] ; then
-        PRINTTEXT+="\n"
-    fi
-
-    printf "%b" "${PRINTTEXT}"
-
-    if (( STATUS >= 1 )); then
-        tput cvvis
-        exit 1
-    fi
-}
-
-run_cmd() {
-    if eval "$@" >> "${LOGFILE}" 2>&1; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-do_task() {
-    print_task "$1" -1 false
-    if run_cmd "$2"; then
-        print_task "$1" 0 true
-    else
-        print_task "$1" 1 true
-    fi
-}
-
-do_function_task() {
-    if ! run_cmd "$1"; then
-        print_task "${MESSAGE}" 1 true
-    fi
-}
-
-do_function_task_retry() {
-    local COUNT=0
-    local RETRY
-    RETRY="$2"
-    local TIMEOUT
-    if [[ -z "$3" ]]; then
-        TIMEOUT="60"
-    else
-        TIMEOUT="$3"
-    fi
-
-    while :
-    do
-        if ! run_cmd "$1"; then
-            COUNT=$(( COUNT + 1 ))
-            print_task "${MESSAGE} (${COUNT})" -3 false
-            sleep "${TIMEOUT}"
-            if (( COUNT == RETRY )); then
-                print_task "${MESSAGE}" 1 true
-            fi
-        else
-            print_task "${MESSAGE}     " -1 false
-            break
-        fi
-    done
-}
-
-do_function_task_if() {
-    if ! run_cmd "$1"; then
-        do_function_task "$2"
-    else
-        do_function_task "$3"
-    fi
-}
-
-do_function() {
-    MESSAGE="$1"
-
-    print_task "${MESSAGE}" -1 false
-    eval "$2"
-    print_task "${MESSAGE}" 0 true
-}
-
 ## *************************************************************************************************** ##
 ##        __  __          _____ _   _                                                                  ##
 ##       |  \/  |   /\   |_   _| \ | |                                                                 ##
@@ -300,13 +179,24 @@ do_function() {
 ##                                                                                                     ##
 ## *************************************************************************************************** ##
 
-echo 'Welcome to Katello installer'
+echo 'Welcome to Repo installer'
 
 ## Check if version is specified
-if [[ $# -eq 2 ]]; then
-    ORG_ID="$1"
-    VERSION="$2"
+if [[ $# -eq 3 ]]; then
+    PASSWORD="$1"
+    ORG_ID="$2"
+    VERSION="$3"
 else
+    echo -n "Password: "
+    read -rs PASSWORD
+    echo
+
+    ## Check if password is specified
+    if [[ -z "${PASSWORD}" ]]; then
+        echo "No password supplied"
+        exit 1
+    fi
+    
     echo -n "Organization ID: "
     read -rs ORG_ID
     echo
@@ -336,6 +226,28 @@ fi
 
 # Hide cursor
 tput civis
+
+# Download functions file
+curl -s https://raw.githubusercontent.com/irjdekker/Katello/master/functions/functions.sh -o "/tmp/functions.sh"
+
+# Source functions file
+if [ -f "/tmp/functions.sh" ]; then
+    source "/tmp/functions.sh"
+else
+    echo "Functions file not available"
+    exit 1
+fi
+
+# Download vault file
+do_function "Download vault file" "do_download_vaultfile"
+
+# Source vault file
+if [ -f "/tmp/$VAULTFILE" ]; then
+    source "/tmp/$VAULTFILE"
+else
+    echo "Vault file not available"
+    exit 1
+fi
 
 ## Create Katello setup for CentOS specific version
 do_function "Create Katello setup for CentOS ${VERSION}" "do_populate_katello \"${ORG_ID}\" \"${VERSION}\""
